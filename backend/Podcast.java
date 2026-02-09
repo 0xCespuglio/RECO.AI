f305a6db0d97921ff48f9062518849f821a56653
domix-developer <85223517+domix-developer@users.noreply.github.com>
2026-02-09 13:53:41 +0100

    backand

--- backend/Podcast.java
+++ backend/Podcast.java
@@ -8,7 +8,7 @@ class Podcast {
     String immagine;
     
     // --- COSTRUTTORE ---
-    Podcast (String nome, String descrizione, String[] tags, double[] pesi, String link, String immagine){
+    Podcast(String nome, String descrizione, String[] tags, double[] pesi, String link, String immagine){
         this.nome = nome;
         this.descrizione = descrizione;
         this.tags = tags;
@@ -17,6 +17,53 @@ class Podcast {
         this.immagine = immagine;
     }
 
-    // --- GETTER E SETTER ---
+    // --- GETTER ---
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public double[] getPesi() {
+        return pesi;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getImmagine() {
+        return immagine;
+    }
+
+    // --- SETTER ---
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
     
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+    
+    public void setPesi(double[] pesi) {
+        this.pesi = pesi;
+    }
+    
+    public void setLink(String link) {
+        this.link = link;
+    }
+    
+    public void setImmagine(String immagine) {
+        this.immagine = immagine;
+    }
 }

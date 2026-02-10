5b8bdc361399a578f940184b6de69d71dff583fa
domix-developer <85223517+domix-developer@users.noreply.github.com>
2026-02-10 10:59:38 +0100

    lettura file csv, tolto i pesi

--- backend/Podcast.java
+++ backend/Podcast.java
@@ -5,16 +5,14 @@ class Podcast {
     String nome;
     String descrizione;
     String[] tags;
-    double[] pesi;
     String link;
     String immagine;
     
     // --- COSTRUTTORE ---
-    Podcast(String nome, String descrizione, String[] tags, double[] pesi, String link, String immagine){
+    Podcast(String nome, String descrizione, String[] tags, String link, String immagine){
         this.nome = nome;
         this.descrizione = descrizione;
         this.tags = tags;
-        this.pesi = pesi;
         this.link = link;
         this.immagine = immagine;
     }
@@ -30,10 +28,6 @@ class Podcast {
 
     public String[] getTags() {
         return tags;
-    }
-
-    public double[] getPesi() {
-        return pesi;
     }
 
     public String getLink() {
@@ -55,10 +49,6 @@ class Podcast {
     
     public void setTags(String[] tags) {
         this.tags = tags;
-    }
-    
-    public void setPesi(double[] pesi) {
-        this.pesi = pesi;
     }
     
     public void setLink(String link) {
